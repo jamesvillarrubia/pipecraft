5a25ca3c272ec3459c5678a95ff2bfa6783a2bbb
James Villarrubia <james.villarrubia@gmail.com>
2025-10-20 17:33:53 -0400

    test: add comment to verify branch protection rules

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,6 +8,8 @@
  * - verbose: Normal + verbose messages (detailed operation info)
  * - debug: Verbose + debug messages (maximum detail for troubleshooting)
  *
+ * Note: This is a test change to verify branch protection rules
+ *
  * The log level can be changed at runtime using --verbose or --debug flags
  * passed to the CLI commands.
  *
